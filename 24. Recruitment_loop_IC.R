9252035a9dd94ec347245c703d213b0b58ff036d
AliciaGR5 <99254799+AliciaGR5@users.noreply.github.com>
2023-09-22 18:17:11 +0200

    24. Recruitment_loop_IC.R

--- 24. Recruitment_loop_IC.R
+++ 24. Recruitment_loop_IC.R
@@ -35,7 +35,8 @@ for (sample in names(allrec)) {
   
   ## PLOTTING TIME! ##
   
-  colores <- c("#7ecdb0", "#eeadb0","#fce1ac", "#a2d7a9", "#dcbada", "#d1a055") # Thanks again, Wes Anderson
+  colores <- c("#7ecdb0", "#eeadb0","#fce1ac", "#a2d7a9", "#dcbada", "#d1a055") # Thanks again, Wes Anderson 
+                                                                                # (now I'm using another film hehehe...hint: we watched it on 2023)
   
   plot[[sample]] <- ggplot(data=projs[[sample]], aes(x=Genome, y=RPKG, fill = Genome)) +
     geom_bar(stat="identity", width=0.8) + 
