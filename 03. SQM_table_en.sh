340dd28186f9bd71112e400d442ffcf16c1f6868
AliciaGR5 <99254799+AliciaGR5@users.noreply.github.com>
2025-08-20 18:51:09 +0200

    03. SQM_table_en.sh

--- 03. SQM_table_en.sh
+++ 03. SQM_table_en.sh
@@ -9,11 +9,12 @@
 
 
 # IMPORTANT: This is the script for working on PICASSO. You should carefully consider how to launch it based on the server you have access to.
-#            If you want more information about SqueezeMeta, I recommend that you read up on it: https://github.com/jtamames/SqueezeMeta
+#            If you want more information about SqueezeMeta, I recommend that you read up on it: https://github.com/jtamames/SqueezeMeta and
+#            https://squeezemeta.readthedocs.io/en/latest/index.html
 
 
 module load squeezemeta/1.6.1post1
 
-SqueezeMeta.pl -m sequential -s Samples_IC42_1.txt -f /mnt/home/users/bio_213_us/agroldan/IC42_1_res -extassembly ./IC42_1_scaffolds.fasta -binners maxbin,metabat2,concoct
+SqueezeMeta.pl -m sequential -s Samples_IC42_1.txt -f <pathway_to_your_files>/IC42_1_res -extassembly ./IC42_1_scaffolds.fasta -binners maxbin,metabat2,concoct
 
 sqm2tables.py IC42_1_SQM IC42_1_SQM/results/tables/
